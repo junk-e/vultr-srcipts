be11bc91a4bf31021d28217962320d2142684920
junk-e <47948104+junk-e@users.noreply.github.com>
2020-06-02 05:16:53 +0900

    [FEAT] Add Pubkey Configuration

--- startup_debian10.sh
+++ startup_debian10.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-# system upgrade
+# Configure public key authentication for root #{{{
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+
+PUB_KEY=`cat << EOF
+ssh-ed25519
+AAAAC3NzaC1lZDI1NTE5AAAAIAgYWFVnhTttW1/t3MfVP0HWh7NUNbZ4SQqCzx4Jlxhc
+junk@Quorra
+EOF
+`
+
+echo $PUB_KEY > /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
+#}}}
+
+# System upgrade
 apt update
 apt -y upgrade
 
@@ -10,8 +25,9 @@ apt -y install git
 # environment variable
 cat << EOF >> /root/.bashrc
 
+# The following environment variables have been added by the startup script.
 export TERM=vt100
 EOF
 
-# clone vultr-scripts
-git clone https://github.com/junk-e/vultr-srcipts.git
+# Clone vultr-scripts
+git clone https://github.com/junk-e/vultr-srcipts.git /root
